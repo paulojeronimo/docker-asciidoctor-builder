2a208d64a0ab6b037530e8ca1f38331b35035c50
Paulo Jerônimo <paulojeronimo@gmail.com>
2020-09-18 06:33:24 -0300

    Added support for tags

--- build.sh
+++ build.sh
@@ -3,7 +3,31 @@ set -eou pipefail
 cd "`dirname "$0"`/.."
 build_dir=build
 html() {
+  local tag
+  while [ "${1:-}" ]; do
+    case "$1" in
+      -t|--tag)
+        shift
+        [ "${1:-}" ] && tag=$1 || {
+          echo "Please, specify the tag!"
+          exit 1
+        }
+        git stash &> /dev/null
+        git tag -l | grep -q "^$tag$" || {
+          echo "Git tag is invalid!"
+          git stash pop &> /dev/null
+          exit 1
+        }
+        git checkout $tag &> /dev/null
+        build_dir=$build_dir/$tag
+    esac
+    shift || break
+  done
   docker-asciidoctor -D $build_dir README.adoc -o index.html
+  if [ "${tag:-}" ]; then
+    git checkout master &> /dev/null
+    git stash pop &> /dev/null || :
+  fi
 }
 gh-pages() {
   [ -d $build_dir ] || { echo "\"$build_dir\" directory does not exists!"; return 1; }
@@ -16,4 +40,15 @@ gh-pages() {
   git commit -m "$msg"
   git push --force $remote_repo master:gh-pages
 }
-"${@:-html}"
+usage() {
+  echo "Usage: $0 <[html] [-<t|-tag> <tag>]|gh-pages>"
+  exit 0
+}
+if [ "${1:-}" ]; then
+  [[ $1 =~ ^- ]] && { task=html; set -- html "$@"; } || task=$1
+else
+  task=html
+fi
+shift || :
+type $task &> /dev/null || usage
+$task ${@:-}
